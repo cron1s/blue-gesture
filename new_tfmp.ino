40d93a72e7a1b14cda872b0a9e11bc2b75f8da82
cron1s <78734417+cron1s@users.noreply.github.com>
2023-04-03 13:45:11 +0200

    Changed code so its pulling to ground with pins

--- new_tfmp.ino
+++ new_tfmp.ino
@@ -1,96 +1,57 @@
-#include <SoftwareSerial.h>
 #include <TFMPlus.h>
-#include "printf.h"
 
-const int LauterPin = 3;
-const int LeiserPin = 4;
-SoftwareSerial bluetooth(10, 11); // RX, TX
-int letzterGueltigerWert = 0;
+const int louderPort = 7;   // Digital pin for louder
+const int quieterPort = 8;  // Digital pin for quieter
+const int maxDistance = 67; // Maximum valid distance
+const int minDistance = 15; // Minimum valid distance
+
+TFMPlus tfmP;
+
+int lastValidDistance = 0; // The last valid distance
+int lastVolume = 0;        // The last volume
 
 void setup()
 {
-    pinMode(LauterPin, OUTPUT);
-    pinMode(LeiserPin, OUTPUT);
-
-    Serial2.begin(115200);
-    delay(20);
-    tfmP.begin(&Serial2);
-
-    bluetooth.begin(9600);
-
-    // Send some example commands to the TFMini-Plus
-    // - - Perform a system reset - - - - - - - - - - -
-    printf("Soft reset: ");
-    if (tfmP.sendCommand(SOFT_RESET, 0))
-    {
-        printf("passed.\r\n");
-    }
-    else
-        tfmP.printReply();
-
-    delay(500); // added to allow the System Rest enough time to complete
-
-    // - - Display the firmware version - - - - - - - - -
-    printf("Firmware version: ");
-    if (tfmP.sendCommand(GET_FIRMWARE_VERSION, 0))
-    {
-        printf("%1u.", tfmP.version[0]); // print three single numbers
-        printf("%1u.", tfmP.version[1]); // each separated by a dot
-        printf("%1u\r\n", tfmP.version[2]);
-    }
-    else
-        tfmP.printReply();
-    // - - Set the data frame-rate to 20Hz - - - - - - - -
-    printf("Data-Frame rate: ");
-    if (tfmP.sendCommand(SET_FRAME_RATE, FRAME_20))
-    {
-        printf("%2uHz.\r\n", FRAME_20);
-    }
-    else
-        tfmP.printReply();
-    // - - - - - - - - - - - - - - - - - - - - - - - -
-    if (tfmP.sendCommand(GET_FIRMWARE_VERSION, 0))
-    {
-        printf("%1u.", tfmP.version[0]); // print three single numbers
-        printf("%1u.", tfmP.version[1]); // each separated by a dot
-        printf("%1u\r\n", tfmP.version[2]);
-    }
-    else
-        tfmP.printReply();
-    // - - Set the data frame-rate to 20Hz - - - - - - - -
-    printf("Data-Frame rate: ");
-    if (tfmP.sendCommand(SET_FRAME_RATE, FRAME_20))
-    {
-        printf("%2uHz.\r\n", FRAME_20);
-    }
-    else
-        tfmP.printReply();
-
-    delay(500);
+    pinMode(louderPort, OUTPUT);
+    pinMode(quieterPort, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop()
 {
-    int abstand = tfmP.getData(tfDist);
-    int volumen = map(abstand, 15, 67, 0, 255);
+    int distance = tfmP.getDistance(); // Get distance in cm
 
-    if (abstand > 67)
+    if (distance > maxDistance || distance < minDistance)
     {
-        volumen = letzterGueltigerWert;
+        distance = lastValidDistance; // Replace invalid distance with last valid distance
     }
     else
     {
-        letzterGueltigerWert = volumen;
+        lastValidDistance = distance; // Store the valid distance
     }
 
-    Serial.print("Abstand: ");
-    Serial.print(abstand);
-    Serial.print("cm, Volumen: ");
-    Serial.println(volumen);
+    int volume = map(distance, minDistance, maxDistance, 0, 255); // Map distance to volume
+    volume = constrain(volume, 0, 255);                           // Limit volume to range of 0-255
 
-    bluetooth.write(volumen); // Senden des Volumens über Bluetooth
+    if (volume != lastVolume) // Volume has changed
+    {
+        if (volume > lastVolume)
+        {                                    // Volume is getting louder
+            digitalWrite(louderPort, LOW);   // Invert and pull to ground
+            digitalWrite(quieterPort, HIGH); // No inversion and not pulled to ground
+        }
+        else
+        {                                   // Volume is getting quieter
+            digitalWrite(louderPort, HIGH); // No inversion and not pulled to ground
+            digitalWrite(quieterPort, LOW); // Invert and pull to ground
+        }
+        lastVolume = volume; // Store the current volume
+    }
 
-    analogWrite(LauterPin, 255 - volumen);
-    analogWrite(LeiserPin, volumen);
-    // delay(10); // optional: um das Messen zu verlangsamen und Rauschen zu reduzieren
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.print(" cm, Volume: ");
+    Serial.println(volume);
+
+    delay(5); // Pause between measurements
 }
