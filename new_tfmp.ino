a938687fdcb13a6fea08ba50aed9265c5aa666c2
cron1s <78734417+cron1s@users.noreply.github.com>
2023-04-14 08:52:47 +0200

    Update new_tfmp.ino

--- new_tfmp.ino
+++ new_tfmp.ino
@@ -14,7 +14,74 @@ void setup()
 {
     pinMode(louderPort, OUTPUT);
     pinMode(quieterPort, OUTPUT);
-    Serial.begin(9600);
+    // Initialisiere den Seriellen Monitor
+    Serial.begin(115200);
+
+    delay(20);                                             // Give port time to initalize
+    printf_begin();                                        // Initialize printf.
+    printf("\r\nTFMPlus Library Example - 10SEP2021\r\n"); // say 'hello'
+
+    Serial2.begin(115200); // Initialize TFMPLus device serial port.
+    delay(20);             // Give port time to initalize
+    tfmP.begin(&Serial2);  // Initialize device library object and...
+                           // pass device serial port to the object.
+
+    // Send some example commands to the TFMini-Plus
+    // - - Perform a system reset - - - - - - - - - - -
+    printf("Soft reset: ");
+    if (tfmP.sendCommand(SOFT_RESET, 0))
+    {
+        printf("passed.\r\n");
+    }
+    else
+        tfmP.printReply();
+
+    delay(500); // added to allow the System Rest enough time to complete
+
+    // - - Display the firmware version - - - - - - - - -
+    printf("Firmware version: ");
+    if (tfmP.sendCommand(GET_FIRMWARE_VERSION, 0))
+    {
+        printf("%1u.", tfmP.version[0]); // print three single numbers
+        printf("%1u.", tfmP.version[1]); // each separated by a dot
+        printf("%1u\r\n", tfmP.version[2]);
+    }
+    else
+        tfmP.printReply();
+    // - - Set the data frame-rate to 20Hz - - - - - - - -
+    printf("Data-Frame rate: ");
+    if (tfmP.sendCommand(SET_FRAME_RATE, FRAME_20))
+    {
+        printf("%2uHz.\r\n", FRAME_20);
+    }
+    else
+        tfmP.printReply();
+    // - - - - - - - - - - - - - - - - - - - - - - - -
+
+    /*  // - - - - - - - - - - - - - - - - - - - - - - - -
+        // The next two commands may be used to switch the device
+        // into I2C mode.  This sketch will no longer receive UART
+        // (serial) data.  The 'TFMPI2C_example' sketch in the
+        // TFMPI2C Library can be used to switch the device back
+        // to UART mode.
+        // Don't forget to switch the cables, too.
+        // - - - - - - - - - - - - - - - - - - - - - - - -
+        printf( "Set I2C Mode: ");
+        if( tfmP.sendCommand( SET_I2C_MODE, 0))
+        {
+            printf( "mode set.\r\n");
+        }
+        else tfmP.printReply();
+        printf( "Save settings: ");
+        if( tfmP.sendCommand( SAVE_SETTINGS, 0))
+        {
+            printf( "saved.\r\n");
+        }
+        else tfmP.printReply();
+        // - - - - - - - - - - - - - - - - - - - - - - - -
+    */
+
+    delay(500); // And wait for half a second.
 }
 
 void loop()
