40d226b76f2e03dffe92f626393c9e0a4eecbf93
cron1s <78734417+cron1s@users.noreply.github.com>
2023-04-21 10:28:49 +0200

    Updated comments to german

--- new_tfmp.ino
+++ new_tfmp.ino
@@ -1,32 +1,28 @@
 #include <TFMPlus.h>
 #include <printf.h>
 
-const int louderPort = 6;   // Digitaler Pin für lauter
-const int quieterPort = 7;  // Digitaler Pin für leiser
+const int louderPort = 6;   // Digitaler Pin für lauter //// Digitaler Pin 2 (PWM) https://www.exp-tech.de/blog/arduino-mega-2560-pinbelegung
+const int quieterPort = 7;  // Digitaler Pin für leiser //// Digitaler Pin 3 (PWM) https://www.exp-tech.de/blog/arduino-mega-2560-pinbelegung
 const int maxDistance = 67; // Maximale gültige Entfernung
 const int minDistance = 15; // Minimale gültige Entfernung
-
 TFMPlus tfmP;
-
-int lastValidDistance = 0; // Die zuletzt gültige Entfernung
-int lastVolume = 0;        // Die zuletzt eingestellte Lautstärke
+int lastValidDistance = 0; // Die letzte gültige Entfernung
+int lastVolume = 0;        // Die letzte Lautstärke
 
 void setup()
 {
     pinMode(louderPort, OUTPUT);
     pinMode(quieterPort, OUTPUT);
-    Serial.begin(115200); // Initialisiere den Seriellen Monitor
-
-    delay(20);                                             // Gib dem Port Zeit zur Initialisierung
-    printf_begin();                                        // Initialisiere printf.
-    printf("\r\nTFMPlus Library Example - 10SEP2021\r\n"); // Sage 'Hallo'
-
-    Serial2.begin(115200); // Initialisiere die serielle Schnittstelle des TFMPlus Geräts.
-    delay(20);             // Gib dem Port Zeit zur Initialisierung
-    tfmP.begin(&Serial2);  // Initialisiere das Gerätebibliotheksobjekt und...
-                           // übergebe die serielle Schnittstelle des Geräts an das Objekt.
-
-    // Sende einige Beispielsbefehle an den TFMini-Plus
+    // Initialisiere den Seriellen Monitor
+    Serial.begin(115200);
+    delay(20);                                              // Gib dem Port Zeit zur Initialisierung
+    printf_begin();                                         // Initialisiere printf.
+    printf("\r\nTFMPlus Library Beispiel - 10SEP2021\r\n"); // Sende 'Hallo'
+    Serial2.begin(115200);                                  // Initialisiere die serielle Verbindung zum TFMPlus Gerät.
+    delay(20);                                              // Gib dem Port Zeit zur Initialisierung
+    tfmP.begin(&Serial2);                                   // Initialisiere das Geräte-Library-Objekt und...
+                                                            // übergebe die serielle Verbindung des Geräts an das Objekt.
+    // Sende einige Beispielbefehle an den TFMini-Plus
     // - - Führe einen Systemreset durch - - - - - - - - - - -
     printf("Soft reset: ");
     if (tfmP.sendCommand(SOFT_RESET, 0))
@@ -35,15 +31,13 @@ void setup()
     }
     else
         tfmP.printReply();
-
     delay(500); // Wartezeit, um dem Systemreset genügend Zeit zum Abschluss zu geben
-
     // - - Zeige die Firmware-Version an - - - - - - - - -
     printf("Firmware-Version: ");
     if (tfmP.sendCommand(GET_FIRMWARE_VERSION, 0))
     {
-        printf("%1u.", tfmP.version[0]); // Gib drei einzelne Zahlen aus
-        printf("%1u.", tfmP.version[1]); // jeweils durch einen Punkt getrennt
+        printf("%1u.", tfmP.version[0]); // Drucke drei einzelne Zahlen
+        printf("%1u.", tfmP.version[1]); // getrennt durch Punkte
         printf("%1u\r\n", tfmP.version[2]);
     }
     else
@@ -59,61 +53,52 @@ void setup()
 
     delay(500); // Wartezeit von einer halben Sekunde.
 }
-
-int16_t tfDist = 0; // Entfernungswert vom TFMPlus Sensor
-
+int16_t tfDist = 0;
 void changevolume(int prozent, bool quiet)
 {
     if (quiet)
     {
-        pinMode(quieterPort, OUTPUT); // Setze den Pin für leiser als OUTPUT
-        pinMode(louderPort, INPUT);   // Setze den Pin für lauter als INPUT
+        pinMode(quieterPort, OUTPUT); // Setze quieterPort als Ausgang
+        pinMode(louderPort, INPUT);   // Setze louderPort als Eingang
     }
     else
     {
-        pinMode(louderPort, OUTPUT); // Setze den Pin für lauter als OUTPUT
-        pinMode(quieterPort, INPUT); // Setze den Pin für leiser als INPUT
+        pinMode(louderPort, OUTPUT); // Setze louderPort als Ausgang
+        pinMode(quieterPort, INPUT); // Setze quieterPort als Eingang
     }
-    delay(350); // Pause von 350ms (relativ)
-
-    pinMode(louderPort, INPUT);  // Setze den Pin für lauter zurück auf INPUT
-    pinMode(quieterPort, INPUT); // Setze den Pin für leiser zurück auf INPUT
+    delay(quiet * 70); // Verzögerung basierend auf dem Wert von quiet
 }
 
 void loop()
 {
-    tfmP.getData(tfDist);  // Lese Entfernungswert in cm vom TFMPlus Sensor
-    int distance = tfDist; // Speichere den Entfernungswert in einer lokalen Variable
-
+    tfmP.getData(tfDist); // Hole die Entfernung in cm
+    int distance = tfDist;
     if (distance > maxDistance || distance < minDistance)
     {
-        distance = lastValidDistance; // Ersetze ungültigen Entfernungswert mit dem letzten gültigen Entfernungswert
+        distance = lastValidDistance; // Ersetze ungültige Entfernung mit der letzten gültigen Entfernung
     }
     else
     {
-        lastValidDistance = distance; // Speichere den gültigen Entfernungswert als letzten gültigen Entfernungswert
+        lastValidDistance = distance; // Speichere die gültige Entfernung
     }
-
-    int volume = map(distance, minDistance, maxDistance, 0, 255); // Mappe den Entfernungswert auf den Volumenwert
-    volume = constrain(volume, 0, 255);                           // Begrenze den Volumenwert auf den Bereich von 0-255
-
-    if (volume != lastVolume) // Wenn sich das Volumen geändert hat
+    int volume = map(distance, minDistance, maxDistance, 0, 255); // Mappe die Entfernung auf die Lautstärke
+    volume = constrain(volume, 0, 255);                           // Begrenze die Lautstärke auf den Bereich von 0-255
+    if (volume != lastVolume)                                     // Lautstärke hat sich geändert
     {
-        int diff = (lastVolume - volume);
+        int diff = (lastVolume - volume); // Berechnet die Differenz zwischen den lastVolume und Volume
         if (diff < 0)
             diff *= -1;
         if (volume > lastVolume)
         {
-            changevolume(diff, true); // Wenn das Volumen erhöht wurde, rufe die Funktion changevolume mit "true" auf
+            changevolume(diff, true); // Ändere die Lautstärke leiser
         }
         else
         {
-            changevolume(diff, false); // Wenn das Volumen verringert wurde, rufe die Funktion changevolume mit "false" auf
+            changevolume(diff, false); // Ändere die Lautstärke lauter
         }
-        lastVolume = volume; // Speichere das aktuelle Volumen als letztes Volumen
+        lastVolume = volume; // Speichere die aktuelle Lautstärke
     }
-
-    printf("Entfernung: %d cm, Volumen: %d\n", distance, volume, tfDist);
+    printf("Entfernung: %d cm, Lautstärke: %d\n", distance, volume, tfDist);
 
     delay(5); // Pause zwischen den Messungen
 }
